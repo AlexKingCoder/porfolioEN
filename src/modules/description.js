ae526e045e912e8c2e08e0218dc313c08c7ce22c
Alex <alex.gil95gsd@gmail.com>
2025-03-11 11:22:30 +0100

    Update description.js

--- src/modules/description.js
+++ src/modules/description.js
@@ -9,18 +9,18 @@ export function createDescription() {
     descriptionDiv.appendChild(descriptionTitle);
   
     const introParagraph = document.createElement("p");
-    introParagraph.textContent = "Hello there! My name is Alex and I'm a full stack developer (with a special passion for the backend). I have a varied and growing experience in all kinds of technologies, from ES6, Mongo, React, and I'm currently studying a specialization in AI focused on the enterprise.";
+    introParagraph.textContent = "My name is Alex. I'm a developer specialized in Backend. I work on self-initiated projects to improve my skills every day.";
     descriptionDiv.appendChild(introParagraph);
   
     const skillsParagraph = document.createElement("p");
-    skillsParagraph.textContent = "I work actively in the development and improvement of various applications. I also have a widely distributed YouTube channel, with which I have acquired social media marketing and outreach skills. In addition, I have worked remotely for a high-impact company (Secretos de la Vida), so I am strong in remote work and team leadership.";
+    skillsParagraph.textContent = "I am a voracious student. My routine is to always break my limits. Excellence is the minimum.";
     descriptionDiv.appendChild(skillsParagraph);
   
     const philosophyParagraph = document.createElement("p");
-    philosophyParagraph.textContent = "I am a well-trained, neat and creative developer. I am committed to a job well done and continuous training. I love working in a team. Being also a graduate in Education, I am passionate about helping my colleagues, both technically and emotionally.";
+    philosophyParagraph.textContent = "I document my work on a daily basis via YouTube and Linkedin, to help and inspire other programmers.";
     descriptionDiv.appendChild(philosophyParagraph);
   
     const closingParagraph = document.createElement("p");
-    closingParagraph.textContent = "You can have a look at my specific projects and skills below. If you want to know more about me, you also have my contact details :)";
+    closingParagraph.textContent = "You can take a look at my specific projects and skills below. If you want to get to know me better, or if you want to propose me a challenge, you can contact me in the email you will find at the end.";
     descriptionDiv.appendChild(closingParagraph);
   }
